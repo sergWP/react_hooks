22d036da3bf63db0016dec4e4dbbd40cd4c3d85d
sergWP <kuzmenko.sergei@gmail.com>
2022-02-11 09:05:21 +0200

    useCallback: мемоизированный колбэк

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
@@ -6,6 +6,15 @@ const Slider = (props) => {
 
     const [slide, setSlide] = useState(0);
     const [autoplay, setAutoplay] = useState(false);
+
+    // Это полезно при передаче колбэков оптимизированным дочерним(!) компонентам,
+    const getSomeImages = useCallback(() => {
+        console.log('useCallback!');
+        return [
+            "https:www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+            "https:www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+        ]
+    }, [])
 
     function loggin() {
         console.log('log!');
@@ -36,7 +45,7 @@ const Slider = (props) => {
     return (
         <Container>
             <div className="slider w-50 m-auto">
-                <img className="d-block w-100" src="https:www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <Slide getSomeImages={getSomeImages} />
                 <div className="text-center mt-5">Active slide {slide}<br/>{autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
                     <button 
@@ -54,6 +63,20 @@ const Slider = (props) => {
     )
 }
 
+// дочерний компонент
+const Slide = ({getSomeImages}) => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        setImages(getSomeImages())
+    }, [getSomeImages])
+
+    return (
+        <>
+            {images.map((url, i) => <img key={i} className="d-block w-100" src={url} alt="slide" />)}
+        </>
+    )
+}
 
 function App() {
     const [slider, setSlider] = useState(true);
